ae72680c96af8a0acf7eb57f9d04e6c7cd334a8f
tsuyoshi-ueda-ri <63494238+tsuyoshi-ueda-ri@users.noreply.github.com>
2020-07-14 17:52:27 +0900

    message-complete

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,18 +9,21 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messagea_path(@group), notice: 'メッセージが送信されました'
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
       render :index
     end
   end
+    def edit
+      @group = Group.find(params[:id])
+    end
 
   private
 
   def message_params
-    params.require(:message).permit(:content, image).merge(user_id: current_user.id)
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 
   def set_group
